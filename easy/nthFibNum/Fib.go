f43c141ca54d7283494b50b4b7d1b28e39aac67c
calgor <ahomayoni684@gmail.com>
2023-02-23 19:20:51 +0330

    nth Fib number with DP

---
+++ easy/nthFibNum/Fib.go
@@ -0,0 +1,13 @@
+package main
+
+var fibNums [10000]int
+
+func Fib(n int) int {
+	if n == 1 || n == 2 {
+		return fibNums[n-1]
+	}
+	if fibNums[n-1] == 0 {
+		fibNums[n-1] = Fib(n-1) + Fib(n-2)
+	}
+	return fibNums[n-1]
+}
